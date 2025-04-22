65688df2d059027503e3ace34ea4c013abf45ca8
험버트씨네 저장소 <lawyeruh@naver.com>
2025-04-23 00:00:34 +0900

    집에서 문제 풀다가.

---
+++ Main1002/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int T = 0, x1 = 0, y1 = 0, x2 = 0, y2 = 0, r1 = 0, r2 = 0;
+	scanf_s("%d", &T);
+
+	for (int i = 0; i < T; i++)
+	{
+		scanf_s("%d %d %d %d %d %d",
+			x1, y1, r1, x2, y2, r2);
+
+	}
+
+	return 0;
+}
